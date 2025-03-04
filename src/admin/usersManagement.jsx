d066d71d7f8f7d5b72adee9902d4c7594d5cf353
Nurdafiqin <Khulika2007@gmail.com>
2025-03-04 16:01:27 +0700

    feat:add integrasi get users

--- src/admin/usersManagement.jsx
+++ src/admin/usersManagement.jsx
@@ -8,111 +8,22 @@ import logoBiodata from "../assets/logoBiodata.png";
 import logoPeringkat from "../assets/logoPeringkat.png";
 import logoRekap from "../assets/logoRekap.png";
 import avatarProfil from "../assets/Group 9.png";
+import axios from "axios";
+import {
+  FaUserEdit,
+  FaTrashAlt,
+  FaSearch,
+  FaPlus,
+} from "react-icons/fa";
 
 const GenerateUser = () => {
-  // const [profile, setProfile] = useState(null);
-  // const [error, setError] = useState(null);
-  // const [loading, setLoading] = useState(true);
-  // const [isEditing, setIsEditing] = useState(false);
-  // const [formData, setFormData] = useState({
-  //   userName: "",
-  //   nisn: "",
-  //   divisi: "",
-  //   class: "",
-  //   address: "",
-  //   avatar: ""
-  // });
-  // const userId = localStorage.getItem("userId");
-
-  // useEffect(() => {
-  //   const fetchProfile = async () => {
-  //     try {
-  //       const response = await fetch(
-  //         `http://localhost:3001/profiles/${userId}`,
-  //         {
-  //           headers: {
-  //             Authorization: `Bearer ${localStorage.getItem("accessToken")}`
-  //           }
-  //         }
-  //       );
-
-  //       if (response.ok) {
-  //         const data = await response.json();
-  //         setProfile(data);
-  //         setFormData({
-  //           userName: data.userName || "", 
-  //           nisn: data.nisn || "",
-  //           divisi: data.divisi || "",
-  //           class: data.class || "",
-  //           address: data.address || "",
-  //           avatar: data.avatar || ""
-  //         });
-  //       } else {
-  //         setProfile(null);
-  //       }
-  //     } catch (error) {
-  //       setError("Terjadi kesalahan saat mengambil data.");
-  //     }
-  //   };
-
-  //   fetchProfile();
-  // }, [userId]);
-
-  // const handleEditProfile = () => {
-  //   setIsEditing(true);
-  // };
-
-  // const handleCancelEdit = () => {
-  //   setIsEditing(false);
-  // };
-
-  // const handleChange = (e) => {
-  //   const { name, value } = e.target;
-  //   setFormData((prevData) => ({
-  //     ...prevData,
-  //     [name]: value
-  //   }));
-  // };
-
-  // const handleSaveProfile = async () => {
-  //   try {
-  //     const response = await fetch(`http://localhost:3001/profiles/${userId}`, {
-  //       method: "PATCH",
-  //       headers: {
-  //         "Content-Type": "application/json",
-  //         Authorization: `Bearer ${localStorage.getItem("accessToken")}`
-  //       },
-  //       body: JSON.stringify(formData)
-  //     });
-
-  //     if (response.ok) {
-  //       const updatedProfile = await response.json();
-  //       setProfile(updatedProfile);
-  //       setIsEditing(false);
-  //     } else {
-  //       setError("Gagal memperbarui profil.");
-  //     }
-  //   } catch (error) {
-  //     setError("Terjadi kesalahan saat memperbarui profil.");
-  //   }
-  // };
-
-
-  // if (error) {
-  //   return (
-  //     <div className="flex h-screen justify-center items-center text-red-500">
-  //       <p>{error}</p>
-  //     </div>
-  //   );
-  // }
-
-  const [absensi, setAbsensi] = useState([]);
+  const [users, setUsers] = useState([]); 
   const [loading, setLoading] = useState(true);
   const [currentPage, setCurrentPage] = useState(1);
   const [perPage] = useState(5);
 
   useEffect(() => {
-    const fetchAbsensi = async () => {
+    const fetchUsers = async () => {
       try {
         const token = localStorage.getItem("accessToken");
 
@@ -120,34 +31,26 @@ const GenerateUser = () => {
           throw new Error("Token tidak ditemukan");
         }
 
-        const userId = getUserIdFromToken(token);
+        const response = await axios.get(`http://localhost:3001/users`, {
+          headers: { Authorization: `Bearer ${token}` },
+        });
 
-        const response = await axios.get(
-          `http://localhost:3001/attendance/user/${userId}`
-        );
-        setAbsensi(response.data);
+        setUsers(response.data);
       } catch (error) {
-        console.error("Gagal mengambil data absensi", error);
+        console.error("Gagal mengambil data user", error);
       } finally {
         setLoading(false);
       }
     };
 
-    fetchAbsensi();
+    fetchUsers();
   }, []);
-
-  const getUserIdFromToken = (token) => {
-    const payload = token.split(".")[1];
-    const decodedPayload = JSON.parse(atob(payload));
-    return decodedPayload.userId;
-  };
 
   const indexOfLastItem = currentPage * perPage;
   const indexOfFirstItem = indexOfLastItem - perPage;
-  const currentAbsensi = absensi.slice(indexOfFirstItem, indexOfLastItem);
+  const currentUsers = users.slice(indexOfFirstItem, indexOfLastItem);
 
   const paginate = (pageNumber) => setCurrentPage(pageNumber);
-
 
   return (
     <div className="flex h-screen">
@@ -158,59 +61,18 @@ const GenerateUser = () => {
           <h2 className="text-xl font-bold mt-4">GoAbsen</h2>
         </div>
         <nav className="flex flex-col space-y-6 text-left w-full px-6">
-          {/* <Link
-            to="/beranda"
-            className="flex items-center gap-3 text-white hover:bg-blue-700 px-4 py-2 rounded-lg"
-          >
-            <img
-              src={logoBeranda}
-              alt="image beranda"
-              className="max-w-full h-auto"
-            />
-            <i className="fas fa-home"></i> Beranda
-          </Link> */}
           <Link
             to="/usersmanage"
             className="flex items-center gap-3 text-white hover:bg-blue-700 px-4 py-2 rounded-lg"
           >
-            <img
-              src={logoBiodata}
-              alt="image beranda"
-              className="max-w-full h-auto"
-            />
+            <img src={logoBiodata} alt="image beranda" className="max-w-full h-auto" />
             <i className="fas fa-user"></i>Users Management
           </Link>
-          {/* <Link
-            to="/goAbsen"
-            className="flex items-center gap-3 text-white hover:bg-blue-700 px-4 py-2 rounded-lg"
-          >
-            <img
-              src={logoAbsen}
-              alt="image beranda"
-              className="max-w-full h-auto"
-            />
-            <i className="fas fa-clipboard-check"></i> GoAbsen
-          </Link> */}
-          {/* <Link
-            to="/peringkat"
-            className="flex items-center gap-3 text-white hover:bg-blue-700 px-4 py-2 rounded-lg"
-          >
-            <img
-              src={logoPeringkat}
-              alt="image beranda"
-              className="max-w-full h-auto"
-            />
-            <i className="fas fa-chart-line"></i> Peringkat
-          </Link> */}
           <Link
             to="/arekap"
             className="flex items-center gap-3 text-white hover:bg-blue-700 px-4 py-2 rounded-lg"
           >
-            <img
-              src={logoRekap}
-              alt="image beranda"
-              className="max-w-full h-auto"
-            />
+            <img src={logoRekap} alt="image beranda" className="max-w-full h-auto" />
             <i className="fas fa-file-alt"></i> Rekap
           </Link>
         </nav>
@@ -224,14 +86,14 @@ const GenerateUser = () => {
 
       {/* Page Content */}
       <div className="flex-1 bg-white p-10">
-        <h1 className="text-3xl font-bold mb-6">Buat Akun Siswa</h1>
+        <h1 className="text-3xl font-bold mb-6">Manajemen User</h1>
         
         <div className="bg-white rounded-lg shadow-md overflow-hidden">
           <div className="flex bg-[#110770] text-white font-semibold place-content-between">
-            <p className=" py-3 px-6 ">Rekap Absensi</p>
+            <p className="py-3 px-6">Daftar User</p>
             <button className="px-6 flex p-1 place-items-center">
-              <img src={logoPlus} alt="logo tambah" className="w-5 h-5 mx-auto" /> 
-              Tambah Akun</button>
+              <FaPlus className="mr-2 bg-blue text-2xl"/>
+            </button>
           </div>
           <table className="w-full table-auto border-collapse">
             <thead>
@@ -239,38 +101,41 @@ const GenerateUser = () => {
                 <th className="text-left py-3 px-4">No</th>
                 <th className="text-left py-3 px-4">Nama</th>
                 <th className="text-left py-3 px-4">Divisi</th>
-                <th className="text-left py-3 px-4">Password</th>
+                <th className="text-left py-3 px-4">Action</th>
               </tr>
             </thead>
             <tbody>
               {loading ? (
                 <tr>
-                  <td colSpan="5" className="text-center py-6">
-                    Loading...
-                  </td>
+                  <td colSpan="3" className="text-center py-6">Loading...</td>
                 </tr>
-              ) : absensi.length === 0 ? (
+              ) : users.length === 0 ? (
                 <tr>
-                  <td colSpan="5" className="text-center py-6 text-gray-500">
-                    Tidak ada data absensi
+                  <td colSpan="3" className="text-center py-6 text-gray-500">
+                    Tidak ada user
                   </td>
                 </tr>
               ) : (
-                currentAbsensi.map((data, index) => (
+                currentUsers.map((user, index) => (
                   <tr
-                    key={data.id}
+                    key={user.id}
                     className={`border-b border-gray-200 ${
                       index % 2 === 0 ? "bg-gray-50" : "bg-white"
                     }`}
                   >
-                    <td className="py-3 px-4">
-                      {indexOfFirstItem + index + 1}
-                    </td>
-                    <td className="py-3 px-4">{data.user.userName}</td>
-                    <td className="py-3 px-4">{data.user.divisi}</td>
-                    <td className="py-3 px-4">{data.status}</td>
-                    <td className="py-3 px-4">
-                      {new Date(data.date).toLocaleDateString()}
+                    <td className="py-3 px-4">{indexOfFirstItem + index + 1}</td>
+                    <td className="py-3 px-4">{user.userName}</td>
+                    <td className="py-3 px-4">{user.divisi}</td>
+                    <td className="py-3 px-4 flex gap-2">
+                      <button className="bg-yellow-500 hover:bg-yellow-600 text-white px-3 py-1 rounded-lg">
+                        <FaUserEdit />
+                      </button>
+                      <button
+                        onClick={() => handleDelete(user.id)}
+                        className="bg-red-600 hover:bg-red-700 text-white px-3 py-1 rounded-lg"
+                      >
+                        <FaTrashAlt />
+                      </button>
                     </td>
                   </tr>
                 ))
@@ -282,7 +147,7 @@ const GenerateUser = () => {
         {/* Pagination */}
         <div className="mt-4 flex justify-center">
           {Array.from(
-            { length: Math.ceil(absensi.length / perPage) },
+            { length: Math.ceil(users.length / perPage) },
             (_, i) => (
               <button
                 key={i + 1}
@@ -302,5 +167,6 @@ const GenerateUser = () => {
     </div>
   );
 };
+
 
 export default GenerateUser;
